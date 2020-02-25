fb8ae8450f9dbedbc3f7ca48d0505d0ede3b39fe
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-02-25 11:05:09 -0600

    CORS permitido

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,13 +13,21 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
+    //protected $middleware = [
+    //    \App\Http\Middleware\TrustProxies::class,
+    //    \App\Http\Middleware\CheckForMaintenanceMode::class,
+    //    \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+    //    \App\Http\Middleware\TrimStrings::class,
+    //    \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    //];
     protected $middleware = [
-        \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-    ];
+        \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\Cors::class,//cors added here 
+         ];
 
     /**
      * The application's route middleware groups.
