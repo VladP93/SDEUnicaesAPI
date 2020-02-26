2d22f2334c4d6b06a4e76ae332759d5c55082b01
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-02-26 02:12:44 -0600

    Control de usuarios login

---
+++ app/Http/Controllers/LogInController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Usuario;
+use Illuminate\Http\Request;
+
+class LogInController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(['Mensaje'=>'Esperando credenciales'],200);
+        
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $usuario = Usuario::where('usuario',$request->usuario)
+        ->where('contrasena',$request->contrasena)
+        ->first();
+        
+        if(!empty($usuario)){
+            return response()->json([true],200);
+        }else{
+            return response()->json([],204);
+        }
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Usuario  $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function show($idusuario)
+    {
+        //$usuario = Usuario::where('usuario',$idusuario)
+        //#->where('contrasena','passa')
+        //->first();
+//
+        //if(!empty($usuario)){
+        //    return response()->json([true],200);
+        //}else{
+        //    return response()->json([],204);
+        //}
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Usuario  $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Usuario  $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Usuario  $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Usuario $usuario)
+    {
+        //
+    }
+}
